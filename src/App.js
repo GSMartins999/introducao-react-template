0ea0cbc7e05ef8671e7d36377893601dc4669624
Giovanni Souza <GSMartins999>
2023-08-06 21:02:03 -0300

    react-1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./img/logo.png";
+import './App.css';
+
+const clicar = () => {
+  alert("Botão foi clicado!");
+}
+
+function App() {
+  return (
+    <body>
+        <div className="container">
+            <h1>Aprenda React na Labenu!</h1>
+            <div>
+              <div>
+                <img className="Logo" src={logo} alt="logo"/>
+              </div>
+              <div>
+                <a href="https://labenu.com.br">Site da Labenu</a>
+              </div>
+              <div>
+                <button onClick={clicar}> Clique aqui! </button>
+              </div>
+            </div>
+          </div>
+    </body>
+    
+  );
+}
+
+export default App;
